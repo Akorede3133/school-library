3783b495d7981cd103cc6b64a52491be1449ad77
akorede <emiolasaheed1@gmail.com>
2023-10-02 10:17:24 +0100

    Create constructor

---
+++ student.rb
@@ -0,0 +1,8 @@
+require_relative './person.rb'
+
+class Student < Person
+  def initialize(id, age, classroom, name = "Uknown", parent_permission = true)
+    super(id, age, name, parent_permission)
+    @classroom = classroom
+  end
+end
