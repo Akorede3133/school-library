de386b3d904d023c5032b0541c874784acda8afe
akorede <emiolasaheed1@gmail.com>
2023-10-03 10:29:04 +0100

    Create the has-many side of Person and Rental (a person has many rentals).

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -10,12 +10,17 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def can_use_services?
     of_age? || @parent_permission
   end
-
+  
+  def add_rental(rental)
+    rental.person = self
+    @rentals.push(rental)
+  end
   def correct_name
     @name
   end
