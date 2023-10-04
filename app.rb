731b32156aeb87ba18600ddac5b419d8243bceb3
akorede <emiolasaheed1@gmail.com>
2023-10-04 12:09:20 +0100

    Create a method that gives instructions to users

--- app.rb
+++ app.rb
@@ -2,4 +2,7 @@ class App
   def print_welcome_message
     puts "Welcome to School Library App!"
   end
+  def give_instruction
+    puts "\nPlease choose an option by entering a number\n1 - list all books\n2 - List all people\n3 - Create a person\n4 - Create a book\n5 - Create a rental\n6 - List all the rentals for a given person id\n7 - Exit"
+  end
 end
