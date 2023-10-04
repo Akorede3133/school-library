fc907ad96a12357eaebf5c6f73c7791722f42f76
akorede <emiolasaheed1@gmail.com>
2023-10-04 12:15:08 +0100

    Handle the creation of person

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 class App
-  attr_accessor :input
+  attr_accessor :input, :person_type
 
   def initialize
     @input = ''
+    @person_type = ''
+    @persons = []
   end
 
   def print_welcome_message
@@ -20,5 +22,53 @@ class App
   def start
     give_instruction
     get_input
+  end
+
+  def execute
+    case input
+      when "3"
+        create_person
+      else
+        start
+    end
+  end
+
+  def create_person
+    print "Do you want to create a student (1) or teacher (2)? [input number]: "
+    @person_type = gets.chomp
+    if person_type == '1'
+      create_student
+    elsif person_type == '2'
+      puts create_teacher
+    end
+  end
+
+  def create_student
+    print "Age: "
+    age = gets.chomp
+    print "Name: "
+    name = gets.chomp
+    print "Has parent permission? [Y/N]: "
+    permission = gets.chomp
+    permission = permission.downcase == 'y' ? true : false
+    classroom = Classroom.new('ss2')
+    new_student = Student.new(age, classroom, name, parent_permission: permission)
+    classroom.add_student(new_student)
+    @persons << new_student
+    puts "Person created successfully"
+    start
+  end
+
+  def create_teacher
+    print "Age: "
+    age = gets.chomp
+    print "Name: "
+    name = gets.chomp
+    print "Specialization: "
+    specialization = gets.chomp
+    new_teacher = Teacher.new(age, specialization, name)
+    @persons << new_teacher
+    puts "Person created successfully"
+    start
   end
 end
